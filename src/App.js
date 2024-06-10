c789a8d8a5eb1d030b0f33e496b400a0c3afe079
Woodson Parker <14338394+woodsonhp@users.noreply.github.com>
2024-06-10 14:07:03 -0600

    Added buttons linking to my social profile

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 
 import './App.css';
 
+import linkedInLogo from './assets/LI-In-Bug.png';
+import githubLogo from './assets/github-mark-white.png';
+import leetcodeLogo from './assets/leetcode_icon.png';
+
+
+
+
 function App() {
   return (
     <div className="App">
@@ -9,6 +16,11 @@ function App() {
         <p> I'm Woodson, and this is my website.
           Updates coming soon!
         </p>
+        <div class="logo-link-container">
+          <a href="https://www.linkedin.com/in/woodson-parker/" className="logo-link"><img src={linkedInLogo} alt="My LinkedIn Profile"/></a>
+          <a href="https://github.com/woodsonhp" className="logo-link"><img src={githubLogo} alt="My GitHub Profile"/></a>
+          <a href="https://leetcode.com/u/woodsonhp/" className="logo-link"><img src={leetcodeLogo} alt="My Leetcode Profile"/></a>
+        </div>
       </header>
     </div>
   );
